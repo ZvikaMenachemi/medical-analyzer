750b2b59ad053cf5ecf06f0e705f3a769d041d29
Zvika <your@email.com>
2026-03-27 16:16:47 +0300

    Fix: TypeScript errors for CI build

--- src/parsers/ocr.ts
+++ src/parsers/ocr.ts
@@ -37,7 +37,8 @@ export async function ocrPdfDoc(
     canvas.height  = viewport.height;
     const ctx      = canvas.getContext('2d')!;
 
-    await page.render({ canvasContext: ctx, viewport }).promise;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    await page.render({ canvasContext: ctx as any, viewport }).promise;
 
     const { data } = await worker.recognize(canvas);
     texts.push(data.text);
